8974eb37aea0165fa8112a6e2457c546daaf681b
Anton Cakste <acakste@e1z1r6p15.42.us.org>
2018-09-22 11:06:45 -0700

    fixed segfault for undefined behaviour

--- srcs/cpy_short.c
+++ srcs/cpy_short.c
@@ -167,7 +167,10 @@ void	strcpy_wflags(char *str, int *count_out, t_conv_flags *flags, va_list ap)
 	j = 0;
 	raw = flags->arg_function(ap, flags);
 	if (raw == NULL)
+	{
 		write(1, "ERROR", 5);
+		return ;
+	}
 	raw_len = ft_strlen(raw);
     precision_handling(flags, &raw_len, raw);
     if (flags->zero == TRUE)
