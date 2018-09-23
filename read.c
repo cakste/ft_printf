2e82394f5165850dfda3ca247a6d20a20c8865a8
Anton Cakste <acakste@e1z1r6p15.42.us.org>
2018-09-22 18:19:36 -0700

    put read into subfunctions

--- read.c
+++ read.c
@@ -34,6 +34,124 @@ t_conv_flags	*init_flag(void)
 	return (flags);
 }
 
+int	read_flags(const char *format, t_conv_flags *flags)
+{
+	int i;
+
+	i = 0;
+	while (format[i] == '0' || format[i] == '#' || format[i] == '-' || format[i] == '+' || format[i] == ' ')
+	{
+		if (format[i] == '0')
+			flags->zero = TRUE;
+		else if (format[i] == '#')
+			flags->sharp = TRUE;
+		else if (format[i] == '-')
+			flags->minus = TRUE;
+		else if (format[i] == '+')
+			flags->plus = TRUE;
+		else if (format[i] == ' ')
+			flags->space = TRUE;
+		if (flags->minus == TRUE)
+			flags->zero = FALSE;
+		i++;
+	}
+	return (i);
+}
+
+int	read_width_precision(const char *format, t_conv_flags *flags)
+{
+	int i;
+
+	i = 0;
+	if (ft_atoi(&format[i]) != 0)
+	{
+		flags->width = TRUE;
+		flags->width_count = ft_atoi(&format[i]);
+		while (format[i] && format[i] >= '0' && format[i] <= '9')
+			i++;
+	}
+	if (format[i] == '.')
+	{
+		i++;
+		flags->precision = TRUE;
+		flags->precision_count = ft_atoi(&format[i]);
+		while (format[i] >= '0' && format[i] <= '9')
+			i++;
+	}
+	return (i);
+}
+
+int	read_modifier(const char *format, t_conv_flags *flags)
+{
+	int i;
+
+	i = 0;
+	if (format[i] == 'h' || format[i] == 'l' || format[i] == 'j' || format[i] == 'z')
+	{
+		flags->modifier = TRUE;
+		if (format[i] == 'h')
+			flags->arg_function = &get_argument_h_mod;
+		else if (format[i] == 'l')
+			flags->arg_function = &get_argument_l_mod;
+		else if (format[i] == 'j')
+			flags->arg_function = &get_argument_j_mod;
+		else if (format[i] == 'z')
+			flags->arg_function = &get_argument_z_mod;
+		i++;
+	}
+	if (format[i] == 'h' && flags->arg_function == get_argument_h_mod)
+	{
+		flags->arg_function = &get_argument_hh_mod;
+		i++;
+	}
+	if (format[i] == 'l' && flags->arg_function == get_argument_l_mod)
+	{
+		flags->arg_function = &get_argument_ll_mod;
+		i++;
+	}
+	return (i);
+}
+
+int	read_conv_char(const char *format, t_conv_flags *flags)
+{
+	int i;
+
+	i = 0;
+	if (format[i] == 's' || format[i]== 'c' || format[i]== 'd' || format[i]== 'i' || format[i]== 'p'
+		|| format[i]== 'o' || format[i]== 'u' || format[i]== 'x')
+	{
+		flags->conversion = format[i];
+		i++;
+	}
+	else if (format[i] == 'S' || format[i]== 'C' || format[i]== 'D' || format[i]== 'I' || format[i]== 'O'
+		|| format[i]== 'U' || format[i]== 'X' || format[i]== '%')
+	{
+		flags->conversion = format[i];
+		if (flags->modifier == FALSE || (flags->arg_function == get_argument_h_mod && format[i] == 'U'))
+			flags->arg_function = &get_argument_caps;
+		i++;
+	}
+	return (i);
+}
+
+int	read_conversion_spec(char *str, const char *format, int *count_out, va_list ap)
+{
+	t_conv_flags	*flags;
+	int				i;
+
+	i = 0;
+	flags = init_flag();
+	i += read_flags(&format[i], flags);
+	i += read_width_precision(&format[i], flags);
+	i += read_modifier(&format[i], flags);
+	i += read_conv_char(&format[i], flags);
+	strcpy_wflags(str, count_out, flags, ap);
+	free(flags);
+	return (i + 1);
+}
+
+/*
+
 // reads the entire conversion spec and sets the flags accordingly. 
 int	read_conversion_spec(char *str, const char *format, int *count_out, va_list ap)
 {
@@ -45,13 +163,10 @@ int	read_conversion_spec(char *str, const char *format, int *count_out, va_list 
 	//what happens with for example multiple spaces?
 	while (format[i] == '0' || format[i] == '#' || format[i] == '-' || format[i] == '+' || format[i] == ' ')
 	{
-		if (format[i] == '0')// && flags->sharp == FALSE)
+		if (format[i] == '0')
 			flags->zero = TRUE;
 		else if (format[i] == '#')
-		{
 			flags->sharp = TRUE;
-			//flags->zero = FALSE;
-		}
 		else if (format[i] == '-')
 			flags->minus = TRUE;
 		else if (format[i] == '+')
@@ -68,7 +183,6 @@ int	read_conversion_spec(char *str, const char *format, int *count_out, va_list 
 		flags->width_count = ft_atoi(&format[i]);
 		while (format[i] && format[i] >= '0' && format[i] <= '9')
 			i++;
-		//write(1, "|",1);
 	}
 	if (format[i] == '.')
 	{
@@ -101,7 +215,6 @@ int	read_conversion_spec(char *str, const char *format, int *count_out, va_list 
 		flags->arg_function = &get_argument_ll_mod;
 		i++;
 	}
-	//find correct conversion
 	if (format[i] == 's' || format[i]== 'c' || format[i]== 'd' || format[i]== 'i' || format[i]== 'p'
 		|| format[i]== 'o' || format[i]== 'u' || format[i]== 'x')
 		{
@@ -110,13 +223,13 @@ int	read_conversion_spec(char *str, const char *format, int *count_out, va_list 
 		}
 	else if (format[i] == 'S' || format[i]== 'C' || format[i]== 'D' || format[i]== 'I' || format[i]== 'O'
 		|| format[i]== 'U' || format[i]== 'X' || format[i]== '%')
-		{
-			flags->conversion = format[i];
-			if (flags->modifier == FALSE || (flags->arg_function == get_argument_h_mod && format[i] == 'U'))
-				flags->arg_function = &get_argument_caps;
-			i++;
-		}
-	/*else
+	{
+		flags->conversion = format[i];
+		if (flags->modifier == FALSE || (flags->arg_function == get_argument_h_mod && format[i] == 'U'))
+			flags->arg_function = &get_argument_caps;
+		i++;
+	}
+	else
 	{
 		ft_printf("Can't find format conversion '%c'. Check usage.\n", format[i]);
 		exit (1);
@@ -132,29 +245,8 @@ int	read_conversion_spec(char *str, const char *format, int *count_out, va_list 
 	printf("precision_count: %zu\n", flags->precision_count);
 	printf("modifier: %d\n", flags->modifier);
 	printf("conversion: %c\n", flags->conversion);*/
-	strcpy_wflags(str, count_out, flags, ap);
+/*	strcpy_wflags(str, count_out, flags, ap);
 	free(flags);
 	return (i + 1);
-}
+}*/
 
-char	*read_format(char *str, const char *format, va_list ap, int *count_out)
-{
-	int	count_in;
-
-	count_in = 0;
-	while (format[count_in] != '\0')
-	{
-		if (format[count_in] == '%')
-		{
-			count_in += read_conversion_spec(str, &format[count_in + 1], count_out, ap);
-		}
-		else
-		{
-			str[(*count_out)] = format[count_in];
-			*count_out += 1;
-			count_in++;
-		}
-	}
-	str[*count_out] = '\0';
-	return (str);
-}
